80c38268bb2f4039f8080fb04a661f3a00dfe84a
gangdonghui327-ux <gangdonghui327@gmail.com>
2026-01-03 11:04:14 +0900

    [Docs] 라우터 폴터 및 AppLayout.jsx 추가

---
+++ frontend/src/app/router.jsx
@@ -0,0 +1,10 @@
+// 라우터 트리 + loader
+
+import { createBrowserRouter, Navigate } from 'react-router'
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: 
+    }
+])
