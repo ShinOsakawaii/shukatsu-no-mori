758d04db253f8a203ac881f8d802936869fb4ab3
gangdonghui327-ux <gangdonghui327@gmail.com>
2026-01-03 13:34:30 +0900

    [Docs] componensts-review 구성 문서 작성

---
+++ frontend/src/components/review/ReviewCommonHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ReviewCommonHeader(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default ReviewCommonHeader;
---
+++ frontend/src/components/review/ReviewFormButtons.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ReviewFormButtons(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default ReviewFormButtons;
