904969efdc5e54646d5ec9b9a133ef8ac5a681d0
keunsol <bigsol0421@gmail.com>
2026-01-06 00:09:20 +0900

    Feat 기업후기 프론트 작업

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ReviewList from './pages/reviews/ReviewList';
+import ReviewDetail from './pages/reviews/ReviewDetail';
+import ReviewForm from './pages/reviews/ReviewForm';
 
 function App() {
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* 목록 조회 */}
+        <Route path="/companies/:companyId/review" element={<ReviewList />} />
+        {/* 새 후기 작성 */}
+        <Route path="/companies/:companyId/review/new" element={<ReviewForm mode="create" />} />
+        {/* 상세 조회 */}
+        <Route path="/companies/:companyId/review/:reviewId" element={<ReviewDetail />} />
+        {/* 수정 및 삭제 */}
+        <Route path="/companies/:companyId/review/:reviewId/edit" element={<ReviewForm mode="edit" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
