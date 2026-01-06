216f9524dd75ff23c440ba11adb5c0720b3da727
cyannmi <seongchanmi@gmail.com>
2026-01-06 22:36:49 +0900

    fix: 에러 해결 중..

--- backend/src/main/java/com/shinosaka/shukatsunomori/backend/controller/CompanyController.java
+++ backend/src/main/java/com/shinosaka/shukatsunomori/backend/controller/CompanyController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/companies")
+@RequestMapping("/companies")
 @RequiredArgsConstructor
 public class CompanyController {
 
